165f36b30a40eb1068580d7df72fa745afbc7cc0
raghadquni <Raghad.ibrahim.elq.js@tuwaiq.edu.sa>
2021-11-09 15:45:33 +0300

    moviekk

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,4 @@
+///////////////////////////////////// problem 1 (fizzBuzz) /////////////////////////////////////
 const fizzBuzz = (n) => {
     for(i = 1 ; i <= n ; i++) {
 
@@ -13,6 +14,8 @@ const fizzBuzz = (n) => {
 }
 };
 
+
+///////////////////////////////////// problem 2 (isPalindrome) /////////////////////////////////////
 
 function isPalindrome(i)
   {
@@ -30,3 +33,58 @@ function isPalindrome(i)
     return true;
      
   }
+
+
+///////////////////////////////////// problem 3 (who is Online?) /////////////////////////////////////
+
+  const friends = [{
+    username: 'David',
+    status: 'online',
+    lastActivity: 10
+  }, {
+    username: 'Lucy', 
+    status: 'offline',
+    lastActivity: 22
+  }, {
+    username: 'Bob', 
+    status: 'online',
+    lastActivity: 104
+  }]
+
+  const whosOnline = (friends) => {
+    let status = {};
+    let online = [];
+    let offline = [];
+    let away = [];
+    friends.forEach((item, i) => {
+      if (item.status == "online" && item.lastActivity <= 10) {
+        online.push(item.username);
+        status.online = online;
+      } else if (item.status == "offline") {
+        offline.push(item.username);
+        status.offline = offline;
+      } else {
+        away.push(item.username);
+        status.away = away;
+      }
+    });
+    return status;
+  }
+
+  
+
+let people = [{
+  name: 'name', age: 20
+}];
+
+  function above24(object) {
+    const people = {}
+
+     for(i = 0 ;i < object.length-1; i++) {
+       if(array[i].age >= 24)
+      people.push(array[i])
+    }
+        return people;
+      }
+    
+  // console.log((people));
